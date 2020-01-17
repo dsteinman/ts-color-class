467dc223387a764051cbcb672f7753e4b36e5081
Dan Steinman <dansteinman@gmail.com>
2020-01-17 08:53:27 -0500

    rm names2hex

--- names2hex.js
+++
@@ -1,8 +0,0 @@
-var Color = require('./color');
-var names2hex = require('./color-names');
-for (var name in names2hex) {
-	var h = names2hex[name]
-	// console.log(h,' -> ',new Color(h).rgb);
-	names2hex[name] = new Color(h).rgb;
-}
-console.log(JSON.stringify(names2hex));
